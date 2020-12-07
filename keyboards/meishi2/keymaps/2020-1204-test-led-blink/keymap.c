40ec99fed4454a4337d18c943f0560e2480b6edb
nezuppo <nezuppo@localdomain>
2020-12-07 11:46:37 +0900

    Changes to be committed: modified:   keymap.c

--- keyboards/meishi2/keymaps/2020-1204-test-led-blink/keymap.c
+++ keyboards/meishi2/keymaps/2020-1204-test-led-blink/keymap.c
@@ -18,29 +18,29 @@
 #define PRO_MICRO_LED_TX D5
 #define PRO_MICRO_LED_RX B0
 
-bool regrun_enter;
+bool led_flag;
 uint16_t regrun_timer = false;
 uint16_t regrun_interval = 1000; // (1000ms == 1s)
 
-enum my_keycodes {
-    TOGGLE_REGRUN = SAFE_RANGE,
-    KEY2,
-    KEY3,
-    KEY4
-};
+//enum my_keycodes {
+//    TOGGLE_REGRUN = SAFE_RANGE,
+//    KEY2,
+//    KEY3,
+//    KEY4
+//};
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
-    LCTL(KC_Z),  LCTL(KC_X),  LCTL(KC_C), LCTL(KC_V) \
-  )
-};
+//const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//  [0] = LAYOUT( /* Base */
+//    LCTL(KC_Z),  LCTL(KC_X),  LCTL(KC_C), LCTL(KC_V) \
+//  )
+//};
 
 void matrix_init_user(void) {
     setPinOutput(PRO_MICRO_LED_TX);
     setPinOutput(PRO_MICRO_LED_RX);
 
-    writePin(PRO_MICRO_LED_TX, !regrun_enter);
-    writePin(PRO_MICRO_LED_RX, regrun_enter);
+    writePin(PRO_MICRO_LED_TX, !led_flag);
+    writePin(PRO_MICRO_LED_RX, led_flag);
 }
 
 void matrix_scan_user(void) {
@@ -53,20 +53,21 @@ void matrix_scan_user(void) {
     }
 }
 
-void led_set_user(uint8_t usb_led) {
+//void led_set_user(uint8_t usb_led) {
+//
+//}
 
-}
+//bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//    switch(keycode){
+//        case TOGGLE_REGRUN:
+//            if (record->event.pressed) {
+//                regrun_enter ^= 1;
+//                regrun_timer = timer_read();
+//
+//                writePin(PRO_MICRO_LED_TX, !regrun_enter);
+//            }
+//            return false;
+//    }
+//    return true;
+//}
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch(keycode){
-        case TOGGLE_REGRUN:
-            if (record->event.pressed) {
-                regrun_enter ^= 1;
-                regrun_timer = timer_read();
-
-                writePin(PRO_MICRO_LED_TX, !regrun_enter);
-            }
-            return false;
-    }
-    return true;
-}
